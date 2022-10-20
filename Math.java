25e9d6cd3e5201af19a9930fd60d3ca990e1764c
Harry Wharton <200219998@aston.ac.uk>
2022-10-20 14:03:22 +0100

    Added max function

---
+++ Math.java
@@ -0,0 +1,21 @@
+public class Math
+{
+
+    /********************************
+    Developer: Harry Wharton
+    University ID: 200219998
+    Function: This function accepts two integers and returns the greatest value.
+    ********************************/
+    public int max(int a, int b)
+    {
+        return (a > b) ? a : b;
+    }
+
+
+
+
+
+
+
+
+}
